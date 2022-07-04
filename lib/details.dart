a672095747e1c82007154fbc5acc1c526c0cc418
Kefalin <tankovic.lukas@yahoo.com>
2022-07-04 18:17:53 +0200

    initial commit

---
+++ lib/details.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Details extends StatefulWidget {
+  final String title, subtitle, isbn, price, image, link;
+  const Details({Key? key, required this.title, required this.subtitle, required this.isbn, required this.price, required this.image, required this.link}) : super(key: key);
+
+  @override
+  State<Details> createState(){
+    return _DetailsState(title, subtitle, isbn, price, image, link); //presun udajov do buildera UI
+  }
+}
+
+class _DetailsState extends State<Details> {
+  late String title;
+  late String subtitle;
+  late String isbn;
+  late String price;
+  late String image;
+  late String link;
+  _DetailsState(this.title, this.subtitle, this.isbn, this.price, this.image, this.link);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.amber
+      ),
+      body:
+      Center(
+      child: Column(
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.all(3.0),
+                child: Image.network(image)
+            ),
+            Container(
+                padding: EdgeInsets.all(5.0),
+                margin: EdgeInsets.all(0.5),
+                child: Text('Subtitle: $subtitle')
+            ),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.all(0.5),
+                child:  Text('ISBN: $isbn')
+            ),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.all(0.5),
+                child: Text('Price: $price')
+            ),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.all(0.5),
+                child: Text(link, style: TextStyle(fontStyle: FontStyle.italic))
+            )
+      ])));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'dart:convert';
+import 'details.dart'; //import obrazovky s detailmi knihy
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'; //package pre pracu s API volaniami
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  initialRoute: '/',
+  routes: {
+    '/home': (context) => Home(),
+  },
+));
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+//trieda obsahujuca detaily knihy
+class Books {
+  String title;
+  String subtitle;
+  String isbn;
+  String price;
+  String image;
+  String link;
+
+  Books({required this.title, required this.subtitle,required this.isbn,required this.price,required this.image, required this.link}); //vsetky tieto polia su povinne
+}
+
+class _HomeState extends State<Home> {
+  List<Books> entries = []; //zoznam vsetkych knih ziskanych cez API volanie
+
+  late TextEditingController _controller; //ovladac vyhladavacieho pola
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  //funkcia na ziskanie dat z API endpointu, value je vyhladavany pojem
+  void getData(value) async {
+    var url =  Uri.parse('https://api.itbook.store/1.0/search/$value');
+    var response = await get(url);
+
+    Map allData = jsonDecode(response.body);
+    int strany = int.parse(allData['total']);
+
+    int celeStrany = strany ~/ 10; //zistenie poctu stran ktore musime prehladat
+    int zvysneStrany = strany % 10; //pocet zaznamov nad celu stranu, kazda strana ma 10 zaznamov
+
+    List data = allData['books'];
+    data.forEach((element) {
+      Map obj = element;
+      entries.add(Books(title: obj['title'], subtitle: obj['subtitle'], isbn: obj['isbn13'], price: obj['price'], image: obj['image'], link: obj['url']));
+    });
+    //podmienka, ak je viac nez jedna strana tak sa postupne zavolaju vsetky strany databazy
+    if (celeStrany > 0){
+      if (zvysneStrany > 0) {celeStrany++;} //ak je zaznamov viac ako nasobok 10 tak sa prehlada este jedna strana
+      for (int page = 2; page <= celeStrany; page++){
+        url =  Uri.parse('https://api.itbook.store/1.0/search/$value/$page');
+        var response = await get(url);
+        allData = jsonDecode(response.body);
+        List data = allData['books'];
+        data.forEach((element) {
+          Map obj = element;
+          entries.add(Books(title: obj['title'], subtitle: obj['subtitle'], isbn: obj['isbn13'], price: obj['price'], image: obj['image'], link: obj['url']));
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bookstore'),
+        backgroundColor: Colors.amber,
+      ),
+        body: Column(
+            children: <Widget>[
+            Container(
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.all(3.0),
+            child: TextField(
+              controller: _controller,
+              //ak sa zmeni vyhladavany pojem, premazu sa vysledky v zozname
+              onChanged: (String value) async {
+                setState((){entries.clear();});
+                },
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+                labelText: 'Zadajte pojem'
+                ),
+              )),
+              //zoznam obsahujuci vysledky
+              //po kliknuti sa dostavame na druhu obrazovku s detailmi
+              Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: entries.length,
+                itemBuilder: (context, index){
+                  return Card(
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(
+                                builder: (context) => Details(title: entries[index].title, subtitle: entries[index].subtitle, isbn: entries[index].isbn, price: entries[index].price, image: entries[index].image, link: entries[index].link)));
+                      },
+                      title: Text(entries[index].title,style: const TextStyle(fontWeight: FontWeight.bold),),
+                      leading: Image.network(entries[index].image), //karta obsahuje aj obrazok, ten sa nacitava podla URI
+                    )
+                  );
+                },
+                )
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(3.0),
+                //floating button, ktory spusta hladanie, mimo focusu na textfield je treba kliknut dvakrat, nevyrieseny bug
+                child: FloatingActionButton(onPressed: () async{
+                  setState((){getData(_controller.text);});},
+                  backgroundColor: Colors.amber,
+                  child: const Icon(Icons.search),),
+              )]
+        )
+    );
+  }
+}
+
+
+
+
