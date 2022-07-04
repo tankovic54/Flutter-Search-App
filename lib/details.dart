745fc13dd5cfe0ea5a148472564a068e9576bc1f
Kefalin <tankovic.lukas@yahoo.com>
2022-07-04 18:24:42 +0200

    uprava komentarov

--- lib/details.dart
+++ lib/details.dart
@@ -11,7 +11,7 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
-  //detaily
+  //detaily knihy
   late String title;
   late String subtitle;
   late String isbn;
